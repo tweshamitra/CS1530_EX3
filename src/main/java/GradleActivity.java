3ac0cefb114795ad4ad4874911c985e6251b4a7a
Forrest Salamida <fms11@pitt.edu>
2017-10-23 13:48:02 -0400

    Code only, tests still needed

---
+++ src/main/java/GradleActivity.java
@@ -0,0 +1,26 @@
+import java.lang.Math;
+
+public class GradleActivity{
+	public static void main(String[] args){
+		if (args.length == 0 || args.length > 1){
+			System.out.println("Enter 1 valid integer");
+			System.exit(1);
+		}
+		int n = 0;
+		try{
+			n = Integer.parseInt(args[0]);
+		} catch (Exception e){
+			System.out.println("Enter an integer");
+			System.exit(1);
+		} 
+		System.out.println("Tri(n) = " + Tri(n) + "\nLazy(n) = " + Lazy(n));
+	}
+	
+	public static int Tri(int num){
+		return ((num * (num + 1)) / 2);
+	}
+	
+	public static int Lazy(int kC){
+		return ((int)(Math.pow(kC, 2) + kC + 2) / 2);
+	}
+}
